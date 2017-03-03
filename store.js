f586ba94e3cd4a068c6885ac49e2f9bc45fa3089
526371075@qq.com <q2670187>
2017-03-03 17:43:18 +0800

    method add

--- store.js
+++ store.js
@@ -6,15 +6,19 @@
 (function(fn){
 
 })(function(){
+
     var fn,
         private_function,
         Store;
-    var LS = window.localStorage;
-    if(!LS){
+
+    let _ls = window.localStorage;
+
+    if(!_ls){
         console.log('your environment does not support localStorage');
         return null;
     }
-    var PF = private_function = {
+
+    let _pf = private_function = {
         _isJson : function(obj){
             return (obj instanceof Object)&&
             Object.prototype.toString.call(obj).toLowerCase() === "[object object]" &&
@@ -28,6 +32,17 @@
         },
         _isId : function(id){
             return true;//TODO check id
+        },
+        _getNewId:function() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
+        },
+        _compare:function(_old,_new) {
+            if(_old.length === _new.length){
+                _old
+            }
         }
     };
 
@@ -38,12 +53,26 @@
     }
 
     Store.prototype = {
+        init:function(new_filded){
+            if(new_filded instanceof Array?(_pr._compare(JSON.parse(_ls.getItem('filed')),new_filded)):false){
+                
+            }
+        },
+        addOne:function(info){
+            let id = null;
+            if(_pf._isJson(info)){
+                id = _pf._getNewId();
+                info.id = id;
+                _ls.setItem(id,_ls._toString(info));
+            }
+            return id?{status:true,message:'add successed',id:id}:{status:false,message:'wrong json input'};
+        },
         /**
          * @function use id get one
          * @param id
          */
         getOne:function(id){
-            
+
         },
         /**
          * @function use info to get all who has this info
